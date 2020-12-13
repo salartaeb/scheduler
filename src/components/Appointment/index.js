32e85a96944fb2722a07f658eac37bba9dde3341
programming <programminpg@Salars-MacBook-Pro-2.local>
2020-12-13 07:29:50 -0800

    added Header and Empty component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.scss'
+import classnames from 'classnames';
+
+export default function Appointment() {
+  return (
+    <article className="appointment"></article>
+  );
+}
