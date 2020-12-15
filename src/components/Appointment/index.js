6b7e9ea94eb3a6f574f37d157c702429b9ba45eb
programming <programminpg@Salars-MacBook-Pro-2.local>
2020-12-15 08:29:47 -0800

    exported to appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import './styles.scss'
 import classnames from 'classnames';
+import Header from "./Header.js"
+import Empty from "./Empty.js"
+import Show from "./Show.js"
 
-export default function Appointment() {
+export default function Appointment(props) {
+ 
   return (
+    <>
     <article className="appointment"></article>
-  );
+    <div className="appointment">
+      <Header time={props.time}/>
+      { props.interview ? 
+        <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : 
+        <Empty />
+      }
+    </div>
+    </>
+  )
 }
