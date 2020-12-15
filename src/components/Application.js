2fc5aae48491b9d2e71008351cf888660b353e02
programming <programminpg@Salars-MacBook-Pro-2.local>
2020-12-15 08:31:24 -0800

    renered appointment function, map over fixtures

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,13 +1,68 @@
-import React, { useState } from "react";
+import React, {
+  useState
+} from "react";
 
 
 import "components/Application.scss";
 
-import DayList from './DayList'
+import DayList from './DayList';
+import Appointment from 'components/Appointment';
 
-
-const days = [
+const appointments = [{
+    id: 1,
+    time: "12pm",
+  },
   {
+    id: 2,
+    time: "1pm",
+    interview: {
+    student: "Lydia Miller-Jones",
+    interviewer: {
+    id: 1,
+    name: "Sylvia Palmer",
+    avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }, 
+  {
+    id: 3,
+    time: "4pm",
+    interview: {
+    student: "Bob Jones",
+    interviewer: {
+    id: 2,
+    name: "Bob Jones",
+    avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }, 
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+    student: "Michael Gem",
+    interviewer: {
+      id: 3,
+      name: "Michael Gem",
+      avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }, 
+  {
+    id: 5,
+    time: "7pm",
+    interview: {
+      student: "Hope Ashenbury",
+      interviewer: {
+        id: 4,
+        name: "Hope Ashenbury",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+     }
+  }
+];
+
+const days = [{
     id: 1,
     name: "Monday",
     spots: 2,
@@ -27,32 +82,35 @@ const days = [
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
 
-  return (
-    <main className="layout">
-      <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList
-  days={days}
-  day={day}
-  setDay={setDay}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
-      </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
-    </main>
-  );
-}
-
+    return ( 
+      <main className = "layout" >
+        <section className = "sidebar" >
+           <img className = "sidebar--centered"
+                src = "images/logo.png"
+                alt = "Interview Scheduler" 
+            />
+          <hr className = "sidebar__separator sidebar--centered"/>
+          <nav className = "sidebar__menu" >
+          <DayList 
+            days = {days}
+            day = {day}
+            setDay = {setDay}
+          /> 
+          </nav> 
+          <img className = "sidebar__lhl sidebar--centered"
+            src = "images/lhl.png"
+            alt = "Lighthouse Labs" 
+            />
+          </section> 
+          <section className = "schedule" > 
+          {
+            appointments.map((appointment) => {
+              return (
+              <Appointment key={appointment.id} {...appointment} /> 
+              )
+            })
+          } 
+          </section> 
+          </main >
+        );
+      }
