520a007b7366a87f02135d1377ac7023cccdb033
programming <programminpg@Salars-MacBook-Pro-2.local>
2020-12-16 20:01:17 -0800

    conditionally rendering form when pressing button using custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,22 +1,34 @@
 import React, {Fragment} from 'react';
 import './styles.scss'
 import classnames from 'classnames';
+import Form from 'components/Appointment/Form';
 import Header from "./Header.js"
 import Empty from "./Empty.js"
 import Show from "./Show.js"
 
+import useVisualMode from 'hooks/useVisualMode';
+
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode (
+    props.interview ? SHOW : EMPTY
+  );
+
  
   return (
     <>
-    <article className="appointment"></article>
-    <div className="appointment">
+    <article className="appointment">
       <Header time={props.time}/>
-      { props.interview ? 
-        <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : 
-        <Empty />
-      }
-    </div>
+      {mode === EMPTY && (<Empty onAdd={transition}/>)}
+      {mode === SHOW && (
+        <Show student={props.student} interviewer={props.interview}/>
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back}></Form>}
+    </article>
     </>
   )
 }
