00e7a99df6d4441eeab7bb0e11b835ae72872ba0
programming <programminpg@Salars-MacBook-Pro-2.local>
2020-12-16 20:00:51 -0800

    refactored for index component

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export default function Empty(props) {
-
+  const CREATE = "CREATE";
   return (
     <main className="appointment__add">
       <img
         className="appointment__add-buttom"
         src="images/add.png"
         alt="Add"
-        onClick={props.onAdd}
+        onClick={()=>props.onAdd(CREATE)}
         />
     </main>
   )
