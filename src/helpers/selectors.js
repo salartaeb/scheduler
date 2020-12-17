8a2fbcf4fe21eb62d2d747519ba98da4e139d3f3
programming <programminpg@Salars-MacBook-Pro-2.local>
2020-12-16 17:48:50 -0800

    adding getInterviewer helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,20 @@
-export default function getAppointmentsForDay(state, day) {
+export  function getAppointmentsForDay(state, day) {
   const dayMatch = state.days.map(day => day.name);
   if (!day || !dayMatch.includes(day)) return [];
 
   return state.days
     .filter(appointment => appointment.name === day)[0]
     .appointments.map(apptId => state.appointments[apptId]);
-}
+};
 
-// module.exports = getAppointmentsForDay;
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+
+  const interviewerInfo = state.interviewers[interview.interviewer];
+  return {
+    student: interview.student,
+    interviewer: interviewerInfo
+  }
+}
