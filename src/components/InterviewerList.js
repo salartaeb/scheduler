75eec69b5d5c190d973556417328506f537147f5
programming <programminpg@Salars-MacBook-Pro-2.local>
2020-12-18 12:20:21 -0800

    recommit, adding propTypes

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
 import './InterviewerList.scss';
+import PropTypes from 'prop-types'
 
 
 
@@ -29,3 +30,9 @@ export default function InterviewerList(props) {
     </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
